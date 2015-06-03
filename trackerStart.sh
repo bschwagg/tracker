031a33bd962208425ac79e3ae112a67ae0b0d32e
bschwagg <brad.schwagler@gmail.com>
2015-06-02 17:50:20 -0700

    Now tracker will start all projects in the config.ini file

--- trackerStart.sh
+++ trackerStart.sh
@@ -37,10 +37,15 @@ fi
 
 
 echo "Starting monitors to watch project directories.."
-#TODO: Read these out of a settings file...
-#Place an entry for every directory of each project worked here
-/usr/bin/python /usr/local/bin/when-changed /home/bschwagg/dev/bookstore/ -v -1 -r -c /opt/tracker/entry.sh bookstore save &
-/usr/bin/python /usr/local/bin/when-changed /home/bschwagg -v -1 -c /opt/tracker/entry.sh homedir save &
+# TODO: This section really needs to be done in python or something more readable..
+config_file=/opt/tracker/config.ini
+projects=`grep -R "^\[.*\]$" ${config_file} | sed 's/\]//g;s/\[//g'`
+for project in ${projects}; 
+do
+    dir_conf=`sed -e "0,/\[${project}\]/d;/Recursive/,\'$'d;s/Directory = //g" ${config_file} | head -n 1`
+	echo "Starting watch on directory ${dir_conf} for project: ${project}"
+	/usr/bin/python /usr/local/bin/when-changed ${dir_conf} -v -1 -r -c /opt/tracker/entry.sh ${project} save &
+done
 
 #Status reporting
 if [[ `ps aux | grep when-changed | grep -v grep` ]] ; then

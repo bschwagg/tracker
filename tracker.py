67d6b61951cbab408c389b9f95c86275f0b3b6d1
bschwagg <brad.schwagler@gmail.com>
2015-06-04 13:45:11 -0700

    prints out all events in the logs as a quick demo

--- tracker.py
+++ tracker.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python
-
-import matplotlib.pyplot as plt
+import re
+from os import walk
 from datetime import datetime
 
-# Dumb test for now...
+logs = []
+for (dirpath, dirnames, filenames) in walk('/opt/tracker/logs'):
+	#filenames = [dirpath + '/' + f for f in filenames] #to take the full path
+	logs.extend(filenames)
+	break
+
+# Dumb test for now. Just dump all the logs for all the projects..
 times = []
 vals = []
-with open('/opt/tracker/logs/bookstore') as f:
-	for l in f:
-		if 'bookstore' in l:
-			l=l.replace(' - bookstore\n','')
-			#t=unix_time_millis( l )
-			t = datetime.strptime(l, '%Y-%m-%d %H:%M:%S')
-			times.append( t )
-			vals.append( 1 )
+for log in logs:
+	with open('/opt/tracker/logs/' + log) as f:
+		print '*** Project:' + log + ' ***'
+		for l in f:
+			m = re.match(r"(.*) - (.*)",l)
+			if m:
+				print 'Event ' + m.group(2) + ' at ' + m.group(1)
+				t = datetime.strptime(m.group(1), '%Y-%m-%d %H:%M:%S')
+				times.append( t )
+				vals.append( 1 )
+
+
+exit(1)
+
+# To plot it!
+import matplotlib.pyplot as plt
 #plt.plot( times, vals, 'ro' )
 plt.bar(times,vals)
 plt.show()
